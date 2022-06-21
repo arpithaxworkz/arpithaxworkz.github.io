eb17d268893e391a5ad44ec876a256b0dd0b4878
ARPITHA KN <arpithaxworkz@gmail.com>
2022-06-21 11:06:12 +0530

    html

---
+++ javascript.js
@@ -0,0 +1,47 @@
+
+        console.log("java script")
+        var a=10;
+        console.log(a)
+        console.log(typeof(a))
+        a="javascript"
+        console.log(a)
+        console.log(typeof(a))
+        var b;
+        console.log(b)
+        console.log(b)
+        console.log(typeof(b))
+        var a;
+        console.log(a)
+
+        console.log("html")
+        var c=20;
+        console.log(c)
+        console.log(typeof(c))
+       
+        d="html"
+        console.log(d)
+        console.log(typeof(d))
+
+        var e;
+        console.log(e)
+        console.log(e)
+        console.log(typeof(e))
+
+        f=true;
+        console.log(f)
+        console.log(typeof(f))
+        g="";
+        console.log(g)
+        console.log(typeof(g))
+    
+            function display(){
+                console.log("calling display function")
+            }
+            function run(){
+                console.log("calling run function")
+            }
+            function myMethod(){
+                console.log("calling mymethod function")
+            }    
+            
+        
